7d06968d442d74e8b2537f6cec56cf236b92f891
Clairie Zhao <clairiezhao@gmail.com>
2025-02-16 11:43:39 -0500

    test read from stdin

--- pa1/src/list/list.c
+++ pa1/src/list/list.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+
+    //scan standard input until session terminates
+    char op;
+    op = scanf("%c", op);
+    int n;
+    scanf("%*c");
+    n = scanf("%d", n);
+    printf("op: %c, num: %d\n", op, n);
+
+
     return 0;
+}
+
+void insert(int n) {
+
+}
+
+void delete(int n) {
+
 }
