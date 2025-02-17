80b793449abd429f33ccb580c5f4acdb406b5773
Clairie Zhao <clairiezhao@gmail.com>
2025-02-17 13:27:23 -0500

    balance passed autograder

--- pa1/src/balance/balance.c
+++ pa1/src/balance/balance.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+    if(argc != 2) return -1;
+
+    char *msg = argv[1];
+    int len = 0, stack_size = 0; 
+    while(msg[len] != '\0') {
+        len++;
+    }
+
+    int *stack = (int*)malloc(len * sizeof(int));
+    char match[5];
+
+    match[('('%5)] = ')';
+    match[('{'%5)] = '}';
+    match[('['%5)] = ']';
+
+    for(int i=0; i<len; i++) {
+        if(msg[i] == '(' || msg[i] == '{' || msg[i] == '[') {
+            //push onto stack
+            stack[stack_size] = msg[i];
+            stack_size++;
+        }
+        else if(msg[i] == ')' || msg[i] == '}' || msg[i] == ']') {
+            if(stack_size == 0 || msg[i] != match[(stack[stack_size-1]%5)]) {
+                printf("%d: %c\n", i, msg[i]);
+                free(stack);
+                return EXIT_FAILURE;
+            }
+            //pop
+            else {
+                //stack[stack_size-1] = NULL;
+                stack_size--;
+            }
+        }
+        else {
+            continue;
+        }
+        
+    }
+
+    if(stack_size != 0) {
+        printf("open: ");
+        while(stack_size != 0) {
+            printf("%c", match[stack[stack_size-1]%5]);
+            stack_size--;
+        }
+        printf("\n");
+        free(stack);
+        return EXIT_FAILURE;
+    }
+
+
+    free(stack);
     return 0;
 }
